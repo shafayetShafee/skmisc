2af45b894b2502f8dda3a9888d82e9fddd9304f9
shafayetShafee <sshafee@isrt.ac.bd>
2023-09-19 02:25:48 +0600

    added output file arg in bib title casing func

--- R/bib_title_to_title_case.R
+++ R/bib_title_to_title_case.R
@@ -2,21 +2,25 @@
 #'
 #' @param bib_file_path character, path to a `.bib` file.
 #'
+#' @param output_bib_file character, path to a `.bib` file that will contain
+#' the transformed bibentries with title-cased title.
+#'
 #' @return `bib_file_path` as a character string, invisibly.
 #'
 #' @examples
+#'
 #' @export
-bib_title_to_title_case <- function(bib_file_path) {
+bib_title_to_title_case <- function(bib_file_path, output_bib_file) {
 
-  if (!is.character(file)) {
+  if (!is.character(bib_file_path)) {
     stop("Invalid file path: Non-character supplied.", call. = FALSE)
   }
 
-  if (as.numeric(file.access(file, mode = 4)) != 0) {
+  if (as.numeric(file.access(bib_file_path, mode = 4)) != 0) {
     stop("Invalid file path: File is not readable.", call. = FALSE)
   }
 
-  if (as.numeric(file.access(file, mode = 2)) != 0) {
+  if (as.numeric(file.access(output_bib_file, mode = 2)) != 0) {
     stop("Invalid file path: File is not writable", call. = FALSE)
   }
 
